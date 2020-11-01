c953d589b37cc74d5a1f60f2afe1efce63e6dd20
Paul Guillon <alentak27@gmail.com>
2020-11-01 22:13:04 +0100

    page details

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -10,7 +10,6 @@ function head(title){
             <!-- JS -->
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-            <script src="assets/js/inc/navbar.inc.js"></script>
             <script src="assets/js/activeNavbar.js"></script>
             `;
 }
